2c891c4361595c0f6c294600f846897a8baa9d4d
stephanieantonio917 <142942685+stephanieantonio917@users.noreply.github.com>
2025-04-20 15:46:02 -0500

    Update server.js-change port # in server js

--- server/server.js
+++ server/server.js
@@ -8,7 +8,8 @@ import pool from './db.js';
 dotenv.config();
 
 const app = express();
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 8080;
+
 
 app.use(cors());
 app.use(express.json());
